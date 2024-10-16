7e8de72406451f694ce8ac5054d715024b1827a7
Mohd Fardeen <frad.fardeen@gmail.com>
2024-10-16 09:50:32 +0000

    add checks for empty string and non numeric characters

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,4 +5,10 @@ describe('test sum function', () => {
     test.concurrent('add comma delimited numbers', async ({ expect }) => {
         expect(sum('1,2,3')).toBe(6)
     })
+    test.concurrent('passing empty string', async ({ expect }) => {
+        expect(sum('')).toBe(0)
+    })
+    test.concurrent('passing non numeric characters', async ({ expect }) => {
+        expect(() => sum('//[,]\na b ')).toThrowError(/Only addition of numbers is supported./)
+    })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 export function sum(numbers: string) {
     const numbersList = numbers.split(/,/)
-    return numbersList.reduce((total, number) => total + Number.parseFloat(number), 0)
+    return numbersList.reduce((total, number) => {
+        //check for empty string
+        if (number.trim().length == 0)
+            return total
+        const parsedNumber = Number.parseFloat(number)
+        //check for NaN
+        if (Number.isNaN(parsedNumber))
+            throw new Error('Only addition of numbers is supported.')
+        return total + Number.parseFloat(number)
+    }, 0)
 }
